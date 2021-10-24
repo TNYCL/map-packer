f7012ebf4172e45618f211310f92a134bbda8c97
TNYCL <73045039+TNYCL@users.noreply.github.com>
2021-10-24 16:25:05 +0300

    Build compiler update

--- map_packer.py
+++ map_packer.py
@@ -17,9 +17,6 @@ root.withdraw()
 VERSION = "0.1"
 DEBUG = True
 
-project_name = ''
-filepath = ''
-
 def success(message):
     print(Fore.GREEN + "+: " + Style.RESET_ALL + message + Fore.RESET)
 
@@ -83,55 +80,98 @@ def download_assets(extract_to='./assets/settings'):
             return debug_traceback()
     return True
 
-def check_world_folders():
+def check_folders():
     if os.path.exists(filepath + "/arts") == False:
         error('"arts" folder not exists.')
-    if os.path.exists(filepath + "/arts/ingame") == False:
+    elif os.path.exists(filepath + "/arts/ingame") == False:
         error('"arts/ingame" folder not exists.')
-    if os.path.exists(filepath + "/arts/keyart.png") == False:
+    elif os.path.exists(filepath + "/arts/ingame/panorama.jpg") == False:
+        error('"arts/ingame/panorama.jpg" file not exists.')
+    elif os.path.exists(filepath + "/arts/ingame/packicon.jpg") == False:
+        error('"arts/ingame/packicon.jpg" file not exists.')
+    elif os.path.exists(filepath + "/arts/keyart.png") == False:
         error('"arts/keyart.png" file not exists.')
-    if os.path.exists(filepath + "/arts/thumbnail.jpg") == False:
+    elif os.path.exists(filepath + "/arts/thumbnail.jpg") == False:
         error('"arts/thumbnail.jpg" file not exists.')
-    if os.path.exists(filepath + "/arts/partnerart.png") == False:
-        error('"arts/partnert.png" file not exists.')
-    if os.path.exists(filepath + "/world.zip") == False:
+    elif os.path.exists(filepath + "/arts/partnerart.png") == False:
+        error('"arts/partnerart.png" file not exists.')
+    elif os.path.exists(filepath + "/world.zip") == False:
         error('"world.zip" file not exists.')
+    else:
+        build_project()
 
-def build():
+def build_project():
     project_path = os.getcwd() + '/projects/' + project_name + '/'
 
     try:
         shutil.copytree('assets/settings', project_path)
     except FileExistsError:
         error('This project already exists.')
-    
-    #copy world folder
+        sys.exit(1)
+
+    #world folder
     try:
         world_file = ZipFile(filepath + '/world.zip', 'r')
-        message('World files are started to processing.')
         world_file.extractall(path=project_path + 'Content/world_template/')
         message('World files are successfully processed.')
     except Exception:
         error("World files couldn't be processed.")
-
-    #copy skin folder
+    worldtemplate_path = project_path + "Content/world_template/"
+    
+    #skin folder
     has_skin_pack = os.path.exists(filepath + "/skins")
-    message('Skin files are processing..')
     if has_skin_pack:
-        shutil.copy(filepath + '/skins', project_path + 'Content/skin_pack/')
+        try:
+            shutil.copytree(filepath + '/skins', project_path + 'Content/skin_pack/', dirs_exist_ok=True)
+            message('Skin files are successfully processed.')
+        except Exception as ex:
+            error(ex)
+    
+    #marketing art
+    photo_count = 0
+    marketingart_path = project_path + "Marketing Art/"
+    for file in os.listdir(filepath + '/arts/ingame'):
+        if file == "packicon.jpg": continue
+        if file == "panorama.jpg": continue
+        shutil.copy(filepath + "/arts/ingame/" + file, marketingart_path + project_name + "_MarketingScreenshot_" + str(photo_count) + ".jpg")
+        photo_count+=1
+    shutil.copy(filepath + "/arts/keyart.png", marketingart_path + project_name + "_MarketingKeyArt.png")
+    shutil.copy(filepath + "/arts/partnerart.png", marketingart_path + project_name + "_MarketingPartnerArt.png")
 
+    #store art
+    photo_count = 0
+    storeart_path = project_path + "Store Art/"
+    for file in os.listdir(filepath + '/arts/ingame'):
+        if file == "packicon.jpg":
+            shutil.copy(filepath + "/arts/ingame/" + file, storeart_path)
+            os.rename(project_path + "Store Art/" + file, storeart_path + project_name + "_packicont_0.jpg")
+            continue
+        if file == "panorama.jpg":
+            shutil.copy(filepath + "/arts/ingame/" + file, storeart_path)
+            os.rename(project_path + "Store Art/" + file, storeart_path + project_name + "_panorama_0.jpg")
+            continue
+        shutil.copy(filepath + "/arts/ingame/" + file, storeart_path)
+        os.rename(project_path + "Store Art/" + file, storeart_path + project_name + "_MarketingScreenshot_" + str(photo_count) + ".jpg")
+        photo_count+=1
+    shutil.copy(filepath + "/arts/thumbnail.jpg", storeart_path)
+    os.rename(project_path + "Store Art/thumbnail.jpg", storeart_path + project_name + "_Thumbnail_0.jpg")
 
 def select_file():
     if(download_assets()):
         message('Select folder needs to be packaged.')
+        global filepath
         filepath = fd.askdirectory(title='Select Folder')
         if filepath != '':
             message('Folder selected: Path => {path}'.format(path=str(filepath)))
-            build()
+            check_folders()
         else:
             error('File not selected.')
 
 if __name__ == "__main__":
     message('Welcome to Map:Packer v.{}'.format(VERSION))
-    project_name = input(Fore.GREEN+'Project Name'+Fore.RESET+': ')
+    global project_name
+    global shorted_project_name
+    project_name = input(Fore.GREEN+'Normal Project Name'+Fore.RESET+': ')
+    shorted_project_name = input(Fore.GREEN+'Shorted Project Name'+Fore.RESET+': ')
+    project_name.replace(" ", "")
     select_file()
